f2d84d5ba1aa195dd418ba75e2689cc8e766a37b
nethranatarajan2007 <nethranatarajan2007@gmail.com>
2025-12-16 16:41:52 +0530

    Add files via upload

---
+++ Grade.py
@@ -0,0 +1,10 @@
+mark= int(input("enter your mark:"))
+if mark>=90:
+     print("Grade A")
+elif mark>=70 and mark<90:
+    print("Grade B")
+elif mark>=50 and mark<70:
+    print("Grade C")
+else:
+    print("Fail")
+    
