bde2d5a9ee4e80e5a0d1f87f656e3c3d8c94d966
nethranatarajan2007 <nethranatarajan2007@gmail.com>
2025-12-16 16:44:52 +0530

    Add files via upload

---
+++ signal.py
@@ -0,0 +1,9 @@
+colour= str(input("enter a signal colour:"))
+if colour=="red":
+    print("Stop")
+elif colour=="orange":
+    print("Wait")
+elif colour=="green":
+    print("Go")
+else:
+    print("invalid colour")
